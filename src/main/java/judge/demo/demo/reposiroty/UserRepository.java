83f39c77bb7a64b4e405f988f10bf33d480f27d2
judgepena <judge.pena@live.com.ph>
2018-05-29 15:44:36 +0100

    Initial commit

---
+++ src/main/java/judge/demo/demo/reposiroty/UserRepository.java
@@ -0,0 +1,9 @@
+package judge.demo.demo.reposiroty;
+
+import judge.demo.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
